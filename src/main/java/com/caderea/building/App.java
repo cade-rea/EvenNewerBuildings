ae70600b884bb24f3e416fee703f234ebc770ad4
Cade Rea <edacade@gmail.com>
2016-03-27 14:23:12 -0500

    initial comit

---
+++ src/main/java/com/caderea/building/App.java
@@ -0,0 +1,16 @@
+package com.caderea.building;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Created by Cade on 3/27/2016.
+ */
+@ComponentScan("com.caderea.building")
+@EnableAutoConfiguration
+public class App {
+    public static void main(String[] args){
+        SpringApplication.run(App.class, args);
+    }
+}
